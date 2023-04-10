9b01a7e36efde9274f0175d50ad904a773a203a2
codethi <docencia.thiago@gmail.com>
2023-04-10 19:56:22 -0300

    feat: comment

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -10,7 +10,7 @@ async function getAddressFromCEP(cep: string): Promise<AddressEnrollment> {
   const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
 
   if (!result.data || result.data.erro) {
-    throw notFoundError();
+    throw notFoundError(); // lança um erro para quem chamou essa função!
   }
 
   const { bairro, localidade, uf, complemento, logradouro } = result.data;
