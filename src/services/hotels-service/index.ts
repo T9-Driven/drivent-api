bc41b9f582a3a025ab277f722e4d398570aa09e7
codethi <docencia.thiago@gmail.com>
2023-04-24 18:31:18 -0300

    refact: remove comments

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -5,12 +5,10 @@ import ticketsRepository from '@/repositories/tickets-repository';
 import { cannotListHotelsError } from '@/errors/cannot-list-hotels-error';
 
 async function listHotels(userId: number) {
-  //Tem enrollment?
   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
   if (!enrollment) {
     throw notFoundError();
   }
-  //Tem ticket pago isOnline false e includesHotel true
   const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
 
   if (!ticket || ticket.status === 'RESERVED' || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
